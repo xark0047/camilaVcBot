d784695ad8dbe06ba927722f922e567de32f2264
xark0047 <84843254+xark0047@users.noreply.github.com>
2021-06-22 12:21:44 +0530

    Update

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,6 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     & ~ filters.edited
 )
 async def start_(client: Client, message: Message):
-    await message.reply_sticker("CAACAgUAAx0CQ8QTWQACCD9gpSGtPso-JueMmC6tCptx2H6VjAACngEAAmpqaFeIQbV46r_aFh8E")
     await message.reply_text(
         f"""<b>Hi {message.from_user.first_name}!
 \nI can play music in your group's voice chat
