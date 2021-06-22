156c6ffbd46118aebb72d4e688e64f4f710a39af
xark0047 <84843254+xark0047@users.noreply.github.com>
2021-06-22 12:53:55 +0530

    Update

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,4 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service @Mystic_music_bot  .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed ')
+  await USER.send_message(message.chat.id,"Hi there, This is a music assistant service @assista_r0bot  .\n\n ❗️ Rules:\n   - No chatting allowed\n   - No spam allowed \n\n 👉 **DON'T SEND GROUP ANY LINK OR USERNAME.**\n\n ⚠️ Disclamer: If you are sending a message here it means @CAT047NOIR will see your message \n    - Don't add this user to secret groups.\n   - Don't Share private info here\n\n **FEEL FREE TO JOIN @Asiansworld**")
+  return
